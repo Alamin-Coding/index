dd72efe16aa26640dba2bda795679f0ee280e7a9
Alamin-Coding <alamin.codelover@gmail.com>
2023-01-12 18:03:56 +0600

    Function add 2

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@
 // ========================
 $(document).ready(function () {
     // Slider start
-    $(".product_slider").slick({
+    $("product_slider").slick({
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
@@ -66,14 +66,19 @@ $(document).ready(function () {
     
     var $bigItem_4 = $('.top_collection_item-4 .display img'); //save big images
     var $smallItem_4 = $('.top_collection_item-4 .thambnail ul li');//save small images
+    
+    var $bigItem_5 = $('.top_collection_item-5 .display img'); //save big images
+    var $smallItem_5 = $('.top_collection_item-5 .thambnail ul li');//save small images
     //save small images
     //save color circles
     var $color_1 = $('.top_collection_item-1 .colors span');
     var $color_2 = $('.top_collection_item-2 .colors span');
     var $color_3 = $('.top_collection_item-3 .colors span');
     var $color_4 = $('.top_collection_item-4 .colors span');
-    // ====================  Item 01 start  ========================//
+    var $color_5 = $('.top_collection_item-5 .colors span');
 
+
+    // ====================  Item 01 start  ========================//
     //click and moseenter function on small image
     //you could delete one eventlistener
       $smallItem_1.on('click mouseenter', function () {
@@ -114,6 +119,69 @@ $(document).ready(function () {
           $bigItem_2.eq($(this).index()).addClass('active');
           $smallItem_2.eq($(this).index()).addClass('active');
       });
-      // ====================  Item 01 end  ========================//
+      // ====================  Item 02 end  ========================//
+  
+    // ====================  Item 03 start  ========================//
+    //you could delete one eventlistener
+      $smallItem_3.on('click mouseenter', function () {
+        //remove active class from all items
+          $bigItem_3.removeClass('active');
+          $smallItem_3.removeClass('active');
+        //add active class to item as small item's index
+          $bigItem_3.eq($(this).index()).addClass('active');
+          $smallItem_3.eq($(this).index()).addClass('active');
+      });
+
+      $color_3.on('click', function () {
+        //remove active class from all items
+          $bigItem_3.removeClass('active');
+          $smallItem_3.removeClass('active');
+        //add active class to item as small item's index
+          $bigItem_3.eq($(this).index()).addClass('active');
+          $smallItem_3.eq($(this).index()).addClass('active');
+      });
+      // ====================  Item 03 end  ========================//
+  
+    // ====================  Item 04 start  ========================//
+    //you could delete one eventlistener
+      $smallItem_4.on('click mouseenter', function () {
+        //remove active class from all items
+          $bigItem_4.removeClass('active');
+          $smallItem_4.removeClass('active');
+        //add active class to item as small item's index
+          $bigItem_4.eq($(this).index()).addClass('active');
+          $smallItem_4.eq($(this).index()).addClass('active');
+      });
+
+      $color_4.on('click', function () {
+        //remove active class from all items
+          $bigItem_4.removeClass('active');
+          $smallItem_4.removeClass('active');
+        //add active class to item as small item's index
+          $bigItem_4.eq($(this).index()).addClass('active');
+          $smallItem_4.eq($(this).index()).addClass('active');
+      });
+      // ====================  Item 04 end  ========================//
+  
+    // ====================  Item 05 start  ========================//
+    //you could delete one eventlistener
+      $smallItem_5.on('click mouseenter', function () {
+        //remove active class from all items
+          $bigItem_5.removeClass('active');
+          $smallItem_5.removeClass('active');
+        //add active class to item as small item's index
+          $bigItem_5.eq($(this).index()).addClass('active');
+          $smallItem_5.eq($(this).index()).addClass('active');
+      });
+
+      $color_5.on('click', function () {
+        //remove active class from all items
+          $bigItem_5.removeClass('active');
+          $smallItem_5.removeClass('active');
+        //add active class to item as small item's index
+          $bigItem_5.eq($(this).index()).addClass('active');
+          $smallItem_5.eq($(this).index()).addClass('active');
+      });
+      // ====================  Item 05 end  ========================//
     
    });
